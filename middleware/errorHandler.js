6c2d674b0848b9d830484e74ceb6a4293caceffe
MrSurenK <surenkworkz@gmail.com>
2023-08-07 06:49:32 +0800

    Imported constant errors into errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,25 +1,31 @@
 // Create custom error handling middleware
 
+// imports constants containing all the error codes
+const { constants } = require("../constants");
+
 const errorHandler = (err, req, res, next) => {
   // if we have a status code then we pass the status code if not we pass the status code of 500
   const statusCode = res.statusCode ? res.statusCode : 500;
   switch (statusCode) {
-   // intercept the response message
-  // err.msg returns an object that is created in ContactController
-    case 400:
+    // intercept the response message
+    // err.msg returns an object that is created in ContactController
+    case constants.VALIDATION_ERROR:
       res.json({
         title: "Validation Failed",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-      case 404:
-      res.json({ title: "Not found", message: err.message, stackTrace: err.stack });
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
       break;
     default:
       break;
   }
-
-
+};
 
 module.exports = errorHandler;
