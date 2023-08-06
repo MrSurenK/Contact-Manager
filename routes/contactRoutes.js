fb498dda6590ba65dbdc66f179b62270206d52a3
MrSurenK <surenkworkz@gmail.com>
2023-08-07 00:46:49 +0800

    All routes set up

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -10,5 +10,25 @@ router.route("/").get((req, res) => {
   res.status(200).json({ message: "Get all contacts" });
 });
 
+// Set up "post" router using the route module
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "Create contact" });
+});
+
+// Set up route to get an individual contact
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+});
+
+// Set up "put" router using the route module
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+});
+
+// Set up "delete" router using the route module
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+});
+
 // Export the router set up above to be used in server.js
 module.exports = router;
