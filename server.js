f1fadc3603bbbfd49d5bc85d295d01252fcb70f0
MrSurenK <surenkworkz@gmail.com>
2023-08-07 06:32:48 +0800

    created custom middleware to handle error

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Import the express module
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler"); //auto imported when called below
 // import dotenv
 const dotenv = require("dotenv").config();
 
@@ -10,12 +11,15 @@ const app = express();
 //Run port in env file via process module or if .env unavaible run the static server at port 5000
 const port = process.env.PORT || 5000;
 
-// Use middlewear(body parser) to parse data stream received from client.
+// Use middleware (body parser) to parse data stream received from client.
 app.use(express.json());
 
 // set up the get route for the API using the "use" middlewear to define contact routes
 // import the folder containing the contact routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
+// Use custom error handler middleware created
+app.use(errorHandler);
 
 // Run the server on the port
 app.listen(port, () => {
