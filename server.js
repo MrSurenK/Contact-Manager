63c9f177b6a8d91a02e1b920b1828b1d6b2a4084
MrSurenK <surenkworkz@gmail.com>
2023-08-07 00:17:13 +0800

    "Express server setup with get route testing"

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("I am in an express project");
+// Import the express module
+const express = require("express");
+// import dotenv
+const dotenv = require("dotenv").config();
+
+// -- Server setup --//
+// Call express server through variable app
+const app = express();
+
+//Run port in env file via process module or if .env unavaible run the static server at port 5000
+const port = process.env.PORT || 5000;
+
+// set up the get route for the API using the following express methods
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
+
+// Run the server on the port
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+// --End of Server set up //
