1a21b025630787d04ab0cd55e532e16d38e77c7f
MrSurenK <surenkworkz@gmail.com>
2023-08-07 06:39:42 +0800

    Added stackTrace to error handling middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,3 +1,10 @@
-const errorHandler = (err, req, res, next) => {};
+// Create custom error handling middleware
+
+const errorHandler = (err, req, res, next) => {
+  // if we have a status code then we pass the status code if not we pass the status code of 500
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  // intercept the response message
+  res.json({ message: err.message, stackTrace: err.stack });
+};
 
 module.exports = errorHandler;
