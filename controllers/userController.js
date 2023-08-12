02bed803c55186c3198c93b4a49928877007ecdc
MrSurenK <surenkworkz@gmail.com>
2023-08-12 15:46:52 +0800

    set up jwt signature and access token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a user
@@ -42,7 +43,36 @@ const registerUser = asyncHandler(async (req, res) => {
 //POST/api/users/login
 //@access public (anyone can access endpoint)
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "login user" });
+  //Fetch email address and password when user login
+  //Destructure email and password from body
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All field are mandatory");
+  }
+  //If there is an email and password check if the user has registered and is in the database
+  const user = await User.findOne({ email });
+  //Once we have the user, compare password with hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        // Payload embeded in token
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      //Set access token secret in env file
+      process.env.ACCESS_TOKEN_SECRET,
+      //Expiry time for access token
+      { expiresIn: "1min" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password is not valid");
+  }
 });
 
 //@desc Get current user
